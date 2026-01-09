485980f6ebab533443f56e615046f8d3ba338b7a
Rick Dawson <dawsora@gmail.com>
2026-01-09 16:21:58 -0500

    Update copyright information for 2026

--- torpy/cell_socket.py
+++ torpy/cell_socket.py
@@ -1,4 +1,5 @@
 # Copyright 2019 James Brown
+# Copyright 2026 Richard Dawson
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
